7c1f2c17aa55715695a423a20d33f4878beb6a55
oscarbyte10 <36045526+oscarbyte10@users.noreply.github.com>
2024-02-16 10:30:47 +0100

    style: Change primaryColor on Ant theme

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,26 +1,28 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LayoutRegister from '../layouts/register';
 import LayoutLogin from '../layouts/login';
 import BuildingList from '../layouts/buildings_list';
-import BuildingMetrics from '../layouts/buildings_metrics'
-import BookmarksList from "../layouts/bookmarks_list";
-
+import BuildingMetrics from '../layouts/buildings_metrics';
+import BookmarksList from '../layouts/bookmarks_list';
+import { Providers } from './Providers';
 
 function App() {
   return (
-    <BrowserRouter basename={"/"}>
-      <Routes>
-        <Route path="/bookmarks" element={<BookmarksList />} />
-        <Route path="/buildings" element={<BuildingList />} />
-        <Route path="/buildings/:buildingId" element={<BuildingMetrics />} />
-        <Route path="/register" element={<LayoutRegister />} />
-        <Route path="home" element={<LayoutRegister />} />
-        <Route path="/login" element={<LayoutLogin />} />
-        <Route path="/" element={<LayoutRegister />} />
-      </Routes>
-    </BrowserRouter>
+    <Providers>
+      <BrowserRouter basename={'/'}>
+        <Routes>
+          <Route path='/bookmarks' element={<BookmarksList />} />
+          <Route path='/buildings' element={<BuildingList />} />
+          <Route path='/buildings/:buildingId' element={<BuildingMetrics />} />
+          <Route path='/register' element={<LayoutRegister />} />
+          <Route path='home' element={<LayoutRegister />} />
+          <Route path='/login' element={<LayoutLogin />} />
+          <Route path='/' element={<LayoutRegister />} />
+        </Routes>
+      </BrowserRouter>
+    </Providers>
   );
 }
 
-export default App
+export default App;
